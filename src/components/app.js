3bbfb942d9ae3b6e7e51d6d9e486217d974a0ae0
pk316 <pr.khiev@gmail.com>
2017-12-19 16:06:19 -0800

    boilerplate class component

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Game from './game';
 
 
 const App = () => (
     <div className="container">
+    <Game/>
     </div>
 );
 
--- src/components/game.js
+++ src/components/game.js
@@ -1,2 +1,23 @@
 import React, { Component } from react;
 import '../game.css';
+
+class Game extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            num : generateRandomNum(),
+            guess : ''
+        }
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Game;
