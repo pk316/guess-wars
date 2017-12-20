82ac37bf6d287f07deff2fc65a536ffcd17cebad
pk316 <pr.khiev@gmail.com>
2017-12-20 10:56:46 -0800

    basic guessing game functionality working

--- src/components/game.js
+++ src/components/game.js
@@ -1,20 +1,68 @@
-import React, { Component } from react;
-import '../game.css';
+import React, { Component } from 'react' ;
+import './game.css';
 
-class Game extends Component{
-    constructor(props){
+class Game extends Component {
+    constructor(props) {
         super(props);
 
         this.state = {
-            num : generateRandomNum(),
-            guess : ''
+            num: this.generateRandomNum(),
+            guess: ''
+        };
+        this.checkGuess = this.checkGuess.bind(this);
+        this.reset = this.reset.bind(this);
+        this.inputChange = this.inputChange.bind(this);
+    }
+    generateRandomNum = () => {
+        return Math.floor(Math.random() * 11)
+    }
+    reset = () => {
+        this.setState(this.state.num)
+    }
+    inputChange(event) {
+        event.preventDefault();
+        this.setState({
+            guess: event.target.value,
+        })
+    };
+    checkGuess = (event) => {
+        event.preventDefault();
+        const { num, guess } = this.state;
+        if (guess == '' || guess == undefined) {
+            return
+        }
+        if (guess > num) {
+            this.setState({
+                response: 'Too high'
+            })
+        } else if (guess < num) {
+            this.setState({
+                response: 'Too Low'
+            })
+        } else {
+            this.setState({
+                response: 'You guessed it!'
+            })
         }
     }
 
     render(){
+        const {num, guess, response} = this.state;
         return (
-            <div>
-
+            <div className="container">
+                <h1 className="text-center my-3">Guessing Game</h1>
+                <form className="form-group">
+                    <div className="row justify-content-center">
+                        <div className="col-4">
+                            <input value={guess} onChange={this.inputChange} className="numInput form-control" type="number" />
+                        </div>
+                        <div className="row justify-content-center ">
+                            <button onClick={this.checkGuess} className="btn btn-lg btn-outline-success col-9">GUESS</button>
+                            <button onClick={this.reset} className="btn btn-lg btn-outline-danger col-9">RESET</button>
+                        </div>
+                    </div>
+                </form>
+                <p>{response}</p>
             </div>
         )
     }
