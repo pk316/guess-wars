525dabc553714744a45cdd25c8ba4c970959ada2
pk316 <pr.khiev@gmail.com>
2018-01-04 15:37:42 -0800

    [edit] removed code no longer needed

--- src/components/previousGuess.js
+++ src/components/previousGuess.js
@@ -3,14 +3,6 @@ import React from 'react';
 
 export default (props) => {
 
-    // const previousGuesses = props.previousGuesses.map((value, index) => {
-    //     return (
-    //         <div key={index}>
-    //             {value}
-    //         </div>
-    //     )
-    // })
-
     const previousGuesses = props.previousGuesses
 
     return (
