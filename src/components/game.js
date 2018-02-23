094d0e90b5c3cbe1f41b069242b8125842b65343
pk316 <pr.khiev@gmail.com>
2018-02-22 20:53:18 -0800

    fixed title animation

--- src/components/game.js
+++ src/components/game.js
@@ -2,13 +2,6 @@
 import React, { Component } from 'react' ;
 import PreviousGuesses from './previousGuess';
 
-const display = {
-    display: 'block'
-};
-const hide = {
-    display: 'none'
-};
-
 class Game extends Component {
     constructor(props) {
         super(props);
@@ -17,7 +10,8 @@ class Game extends Component {
             num: this.generateRandomNum(),
             guess: '',
             previousGuesses: [],
-            toggle: false
+            toggle: false,
+            isEnabled: true
         };
         this.checkGuess = this.checkGuess.bind(this);
         this.reset = this.reset.bind(this);
@@ -28,7 +22,7 @@ class Game extends Component {
         return Math.floor(Math.random() * 1) + 1
     }
     reset = () => {
-        this.setState(this.state)
+        this.setState(this.state);
     }
     inputChange(event) {
         event.preventDefault();
@@ -39,7 +33,7 @@ class Game extends Component {
     checkGuess = (event) => {
         event.preventDefault();
         const { num, guess } = this.state;
-        if (guess == '' || guess == undefined) {
+        if (guess == '' || guess == undefined || guess > 100) {
             return
         }
         if (guess > num) {
@@ -56,10 +50,12 @@ class Game extends Component {
             })
         } else {
             this.setState({
+                winner: "You've guessed the number!",
+                isEnabled: false,
                 response: guess + ' is the correct number!'
             }, () => {
                 this.previousGuesses()
-                this.winner()
+                // this.winner()
             })
         }
     }
@@ -69,35 +65,37 @@ class Game extends Component {
                 previousGuesses: [response, ...previousGuesses],
                 guess: ''
             })
-
     }
-    toggle() {
-        this.setState(prevState => ({
-            toggle: !prevState.toggle
-        }));
-    }
-    winner = () => {
-        this.toggle();
-    }
+    // toggle() {
+    //     this.setState(prevState => ({
+    //         toggle: !prevState.toggle
+    //     }));
+    // }
+    // winner = () => {
+    //     this.toggle();
+    // }
+    
 
     render(){
-        const {num, guess, response, previousGuesses } = this.state;
+        const {num, guess, response, previousGuesses, toggle, winner, isEnabled } = this.state;
+        const display = {display: 'block'};
+        const hide = {display: 'none'};
         return (
             <div className="container">
                 <h1 className="game-title text-center my-3">Guess Wars</h1>
+                <div className="win-div">
+                    <h1 className="win-response">{winner}</h1>
+                </div>
                 <form className="form-group">
-                    <input value={guess} onChange={this.inputChange} className="input-lg text-center" type="number" placeholder="1 - 10" min="0" max="10" autoFocus="autofocus" />
+                    <input value={guess} onChange={this.inputChange} className="input-lg text-center" type="number" placeholder="1 - 100" min="0" max="100" autoFocus="autofocus" />
                     <div className="row justify-content-center">
-            
-                        <button onClick={this.checkGuess} className="btn btn-lg btn-outline-success m-3 text-center">GUESS</button>
+                        <button disabled={!isEnabled} onClick={this.checkGuess} className="btn btn-lg btn-outline-success m-3 text-center">GUESS</button>
                         <button onClick={this.reset} className="btn btn-lg btn-outline-danger m-3 text-center">RESET</button>
                     </div>
                 </form>  
                 <div className="intro">
                     <div className="intro-content">
                         <p className="intro-text">Can you guess correct number ??</p>
-
-                            {/* <PreviousGuesses previousGuesses={previousGuesses}/> */}
                     </div>
                     <div className="guess">
                         <div className="guess-content">
@@ -105,14 +103,14 @@ class Game extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="win-modal" style={this.state.toggle ? display : hide}>
-                    <span onClick={this.reset} className="close-btn">&times;</span>
+                {/* <div className="win-modal" style={this.state.toggle ? display : hide}>
+                    <span onClick={this.close} className="close-btn">&times;</span>
                     <div className="modal-content">
                         <div>
                             <p>You Guessed it!!</p>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         )
     }
